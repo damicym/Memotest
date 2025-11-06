c17c68d022d96f8aa28ad22b869482d9d8905127
Dami <damiancym05@gmail.com>
2025-11-06 20:10:40 -0300

    agrego rogue

--- src/components/Opciones.jsx
+++ src/components/Opciones.jsx
@@ -40,7 +40,7 @@ function Opciones({ totalGroups, setTotalGroups, prevValuePairs, gameMode, setGa
         setAnimationClass("slide-exit")
         setGameMode(prev => {
             switch (prev){
-                case GAME_MODES.CLASSIC: return GAME_MODES.SEQUENCE
+                case GAME_MODES.CLASSIC: return GAME_MODES.ROGUE
                 break
                 case GAME_MODES.ROGUE: return GAME_MODES.SEQUENCE
                 break
